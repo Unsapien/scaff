3e97c4bf93221380c1aec24915fa2923a6d32b23
Gareth Davies <g.davies888@gmail.com>
2020-01-16 11:51:42 +0000

    initial sync of new project

---
+++ mymod.py
@@ -0,0 +1,12 @@
+# this is a module - I want to stop people executing it directly
+# when imported, the value of __name__ will be the module name (not '__main__')
+import sys
+
+
+if __name__ == 'main':
+    sys.exit()
+
+def myfunc():
+    pass
+
+print(__name__)
---
+++ myscript.py
@@ -0,0 +1,6 @@
+
+print('hello world')
+
+
+print('where is all the rum gone?')
+print('where is all the rum gone?')
